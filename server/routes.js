c754f930773d6ffebbad954bb2d2120d3eb7a703
Walter Anderson <walteranderson19@gmail.com>
2014-10-07 17:18:02 -0500

    adding root test route and npm start for heroku

--- server/routes.js
+++ server/routes.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
   app.use('/api/tasks', auth.ensureAuth(), require('./api/task'));
   app.use('/api/users', require('./api/user'));
   app.use('/auth', require('./auth'));
+
+  app.use('/', function(req, res) {
+    res.send('hello world');
+  });
 };
