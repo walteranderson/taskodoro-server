d21a0985db4b18b510387160b2c54015be17623a
Walter Anderson <walteranderson19@gmail.com>
2015-02-27 18:26:56 -0600

    adding Authorization to Allow-Headers

--- app/routes.js
+++ app/routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
   // Allow CORS - see http://enable-cors.org/server_expressjs.html
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
 
